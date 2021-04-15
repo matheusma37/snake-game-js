1dfd1beab03c9d2fdb3f5f6a3069fb60449c85d4
matheusma37 <mtsma37@gmail.com>
2021-04-14 22:29:17 -0300

    Cria a funcionalidade de 'Game Over'

--- src/js/script.js
+++ src/js/script.js
@@ -46,6 +46,13 @@ function iniciarJogo() {
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
+  for (let index = 1; index < snake.length; index++) {
+    if (snake[0].x == snake[index].x && snake[0].y == snake[index].y) {
+      clearInterval(jogo);
+      alert('Game Over :*(')
+    }
+  }
+
   criarBG();
   criarCobrinha();
   desenhaComida();
