11023d4a80b25cea4b5c16199ee0269a59dd8658
matheusma37 <mtsma37@gmail.com>
2021-04-14 21:43:07 -0300

    Cria comida

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,11 @@ snake[0] = {
 
 let direction = 'right';
 
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -21,6 +26,11 @@ function criarCobrinha() {
   }
 }
 
+function desenhaComida() {
+  context.fillStyle = 'red';
+  context.fillRect(food.x, food.y, box, box)
+}
+
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -31,13 +41,14 @@ function update(event) {
 }
 
 function iniciarJogo() {
-  if (snake[0].x > 16 * box && direction == 'right') snake[0].x = 0;
+  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
   if (snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
-  if (snake[0].y > 16 * box && direction == 'down') snake[0].y = 0;
+  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
   if (snake[0].y < 0 * box && direction == 'up') snake[0].y = 16 * box;
 
   criarBG();
   criarCobrinha();
+  desenhaComida();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
